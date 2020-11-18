3f8baacccbe3f809f8bf3fa7bbf169402ab239e4
Kent Brokman <nik08051990@mail.ru>
2020-11-18 21:01:22 +0300

    Updates

---
+++ precache-manifest.20b68e75f8d9d30751591a37d6f230e1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ee5aca07505ea08c2656f1dc84e8f1",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "b0122f5cad0450371838",
+    "url": "/social-network/static/css/main.d0590066.chunk.css"
+  },
+  {
+    "revision": "e8794ed366686582b9b6",
+    "url": "/social-network/static/js/2.379c0312.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.379c0312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0122f5cad0450371838",
+    "url": "/social-network/static/js/main.d9afc3ed.chunk.js"
+  },
+  {
+    "revision": "a4ef0ab68e784a8ff62f",
+    "url": "/social-network/static/js/runtime-main.08d4c5e7.js"
+  },
+  {
+    "revision": "a70e00c547d926bfd2d59365d78c4ee4",
+    "url": "/social-network/static/media/arrow.a70e00c5.svg"
+  },
+  {
+    "revision": "44014b2c1ff6413de7573b2aa2b355a6",
+    "url": "/social-network/static/media/moon.44014b2c.svg"
+  },
+  {
+    "revision": "4956df626e7c7be94630b869045ab865",
+    "url": "/social-network/static/media/preloaderLarge.4956df62.svg"
+  },
+  {
+    "revision": "1336a69b3b60ec204ed4d6685c3dfdfb",
+    "url": "/social-network/static/media/preloaderSmall.1336a69b.svg"
+  },
+  {
+    "revision": "2e6b125fbb08f7d2b90049bb4fedf7e4",
+    "url": "/social-network/static/media/randomUser1big.2e6b125f.png"
+  },
+  {
+    "revision": "47b15aceb7be73e2c5aa4e1f4a915fc1",
+    "url": "/social-network/static/media/ticTacToe.47b15ace.jpg"
+  }
+]);
